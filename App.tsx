b5b2bb590efd59ec1e23fde858af4f88208630b6
talmo777 <76654788+talmo777@users.noreply.github.com>
2026-02-05 23:30:40 +0900

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -6,6 +6,10 @@ import ContestListPage from './pages/ContestListPage';
 import CalendarPage from './pages/CalendarPage';
 import GuidePage from './pages/GuidePage';
 import FeedbackPage from './pages/FeedbackPage';
+import IcPblMileagePage from './pages/benefits/IcPblMileagePage';
+import BigoMileageScholarshipPage from './pages/benefits/BigoMileageScholarshipPage';
+import StartupClubSupportPage from './pages/benefits/StartupClubSupportPage';
+
 
 const App: React.FC = () => {
   return (
@@ -16,6 +20,9 @@ const App: React.FC = () => {
           <Route path="/contests" element={<ContestListPage />} />
           <Route path="/calendar" element={<CalendarPage />} />
           <Route path="/guide" element={<GuidePage />} />
+          <Route path="/benefits/icpbl-mileage" element={<IcPblMileagePage />} />
+          <Route path="/benefits/bigo-mileage-scholarship" element={<BigoMileageScholarshipPage />} />
+          <Route path="/benefits/startup-club-support" element={<StartupClubSupportPage />} />
           <Route path="/feedback" element={<FeedbackPage />} />
           {/* Fallback */}
           <Route path="*" element={<Navigate to="/" replace />} />
