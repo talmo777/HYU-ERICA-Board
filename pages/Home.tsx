7eac2785b66400d963465c2801c9f6c59939d48a
talmo777 <76654788+talmo777@users.noreply.github.com>
2026-02-10 23:08:45 +0900

    Update Home.tsx

--- pages/Home.tsx
+++ pages/Home.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useState } from "react";
+import React, { useEffect, useMemo, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { ChevronRight, ChevronLeft, ArrowRight } from "lucide-react";
 import ContestCard from "../components/ContestCard";
